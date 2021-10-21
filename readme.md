cc4b246de4bfda31cb0c7488e2c76a7dff5d9efe
iser97 <1156653418@qq.com>
2021-10-21 19:56:44 +0800

    master

--- readme.md
+++ readme.md
@@ -2,10 +2,11 @@
 
 ## file dir
 
-cache: root for saving data
+cache: root for saving data, the 'test_data.txt', 'train_data.txt', 'test_label.txt', 'train_label.txt' is save in this directory.
+
 scripts: total scripts
 
-training and test run the following bash 
+training and test run the following command
 ```bash
 python main.py --lr 0.0001 --batch_size 256 --n_epochs 200 --data_split_dim 4 --data_dimension 8 --n_heads 4
 ```
--- scripts/data/mnist_8_8.py
+++ scripts/data/mnist_8_8.py
@@ -7,9 +7,9 @@ from torchvision.transforms.transforms import Resize
 
 kwargs = {'num_workers': 1, 'pin_memory': True}
 batch_size = 1000
-
+mnist_data_root = '/home/zjh/python_program/data/' # change by yourself situation
 train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
+    datasets.MNIST(mnist_data_root, train=True, download=True,
                 transform=transforms.Compose([
                     transforms.Resize([8, 8]),
                     transforms.ToTensor(),
@@ -18,7 +18,7 @@ train_loader = torch.utils.data.DataLoader(
     batch_size=batch_size, shuffle=False, **kwargs)
 
 test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.Compose([
+    datasets.MNIST(mnist_data_root, train=False, transform=transforms.Compose([
                     transforms.Resize([8, 8]),
                     transforms.ToTensor(),
                     transforms.Normalize((0.1307,), (0.3081,))
@@ -38,10 +38,10 @@ for batch_idx, (data, target) in enumerate(train_loader):
 train_data = torch.stack(train_data)
 train_data = train_data.reshape(-1, 8*8)
 train_data = train_data.tolist()
-with open('train_data.txt', mode='w', encoding='utf-8') as w:
+with open('../../cache/train_data.txt', mode='w', encoding='utf-8') as w:
     train_data = json.dumps(train_data)
     w.writelines(train_data)
-with open('train_label.txt', mode='w', encoding='utf-8') as w:
+with open('../../cache/train_label.txt', mode='w', encoding='utf-8') as w:
     train_label = json.dumps(train_label)
     w.writelines(train_label)
 
@@ -54,9 +54,9 @@ for batch_idx, (data, target) in enumerate(test_loader):
 test_data = torch.stack(test_data)
 test_data = test_data.reshape(-1, 8*8)
 test_data = test_data.tolist()
-with open('test_data.txt', mode='w', encoding='utf-8') as w:
+with open('../../cache/test_data.txt', mode='w', encoding='utf-8') as w:
     test_data = json.dumps(test_data)
     w.writelines(test_data)
-with open('test_label.txt', mode='w', encoding='utf-8') as w:
+with open('../../cache/test_label.txt', mode='w', encoding='utf-8') as w:
     test_label = json.dumps(test_label)
     w.writelines(test_label)
